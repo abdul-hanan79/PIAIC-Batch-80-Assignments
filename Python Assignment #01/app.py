3911ded0a4a491f8753c16d92a4b3460c9b20669
Abdul Hanan <abdulhanan3279@gmail.com>
2025-09-10 14:43:59 +0500

    Feat: working on assigmnent #01

---
+++ Python Assignment #01/app.py
@@ -0,0 +1,30 @@
+# Answer 1
+principal = 5000 
+rate = 8
+time = 1
+amount = principal * (1 + rate / 100) ** time
+print("Answer 1: Amount after 1 year:", amount)
+
+
+# Answer 2
+radius=7
+height=10
+total_surface_area = 2 * 3.14 * radius**2 + 2 * 3.14 * radius * height
+print("Answer 2: Total Surface Area of the Cylinder:", total_surface_area)
+
+# Answer 3
+hours=2
+minutes=46
+seconds=40
+total_seconds=(hours*60*60)+(minutes*60)+seconds
+print("Answer 3: Total Seconds:", total_seconds)
+
+
+# Answer 4
+total_bill = 2750
+tip = 10
+number_of_people = 5
+tip_amount = (tip / 100) * total_bill
+total_amount = total_bill + tip_amount
+amount_per_person = total_amount / number_of_people
+print("Answer 4: Amount each person should pay:", amount_per_person)
